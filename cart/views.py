75fa54e8300591027f698284b9d3084a0c5eae30
hoang <hoangnv2782001@gmail.com>
2023-03-12 21:30:12 +0700

    shopping cart

--- cart/views.py
+++ cart/views.py
@@ -11,6 +11,7 @@ def show_cart(request, template_name="cart/cart.html"):
         if postdata['submit'] == 'Update':
             cart.update_cart(request)
         cart_items = cart.get_cart_items(request)
+    cart_items = cart.get_cart_items(request)
     page_title = 'Shopping Cart'
     cart_subtotal = cart.cart_subtotal(request)
     return render(request,template_name, locals())
